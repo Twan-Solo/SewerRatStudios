0a25282fa5177cec7fdda83be864410188f13d64
DavisC54 <dlcerniglia@gmail.com>
2026-02-22 00:38:50 -0500

    Added enemy Ai and stun gun system

---
+++ SewerRatEscape/Assets/Scripts/Enemystate.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// Abstract base for all enemy states. EnemyAI manages transitions between these.
+/// </summary>
+public abstract class EnemyState
+{
+    protected EnemyAI enemy;
+
+    public EnemyState(EnemyAI enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public abstract void Enter();
+    public abstract void Execute();
+    public abstract void Exit();
+}
